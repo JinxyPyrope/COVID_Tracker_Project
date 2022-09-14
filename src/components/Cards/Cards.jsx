6a677dad5ab64739d5f611e4cf964f88b4ba782a
Nhoj <nhojplummer@yahoo.com>
2022-09-13 20:37:31 -0400

    Adding the first Card to the tracker

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react"
+import { Card, CardContent, Typography, Grid } from "@material-ui/core"
 
-const Cards = (props) => {
-  console.group(props);
+import styles from "./Cards.module.css"
 
-  return  (
-    <h1>Cards</h1>
+const Cards = props => {
+  console.group(props)
+
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of active cases of COVID19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
-export default Cards;
+export default Cards
